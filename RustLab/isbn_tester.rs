5dee40640b400b837dfd1c327df10425c5828e34
SantiagoGuty <santiago.gutierrezmorales@valpo.edu>
2024-01-03 02:42:36 -0500

    Pushed the RustLab     An ISBN tester made in Rust.

---
+++ RustLab/isbn_tester.rs
@@ -0,0 +1,47 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func ISBN() {
+	var resultado string
+	var x int = 10
+	var suma int = 0
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for i := 0; i < x-1; i++ {
+
+		if (i == 1) || (i == 3) || (i == 9) {
+			num := r.Intn(10) - 1
+			resultado += fmt.Sprint("-", num)
+			suma += (i + 1) * num
+		} else {
+			num := r.Intn(10) - 1
+			resultado += fmt.Sprint(num)
+			suma += (i + 1) * num
+		}
+	}
+
+	divideSum := suma % 11
+	if divideSum < 10 {
+		resultado += fmt.Sprint("-", divideSum)
+	} else {
+		resultado += fmt.Sprint("-X")
+	}
+
+	fmt.Print("\nISBN: ", resultado, "\n")
+	fmt.Println("\ndivido: ", divideSum)
+	fmt.Println("\nsuma: ", suma)
+
+}
+func main() {
+	ISBN()
+}
